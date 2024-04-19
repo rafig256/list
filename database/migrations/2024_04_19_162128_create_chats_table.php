58f9e7e167660276226990a39c36217c7200eb6c
rafig256 <rafig_256@yahoo.com>
2024-04-19 22:44:03 +0330

    add live chat icon with fixed pisistion in master template

--- database/migrations/2024_04_19_162128_create_chats_table.php
+++ database/migrations/2024_04_19_162128_create_chats_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
-            $table->
+
             $table->timestamps();
         });
     }
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -21,6 +21,8 @@
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
     <!--    toastr  -->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" class="rel">
+    <!-- chat -->
+    <link rel="stylesheet" href="{{asset('frontend/css/chat.css')}}">
     @stack('css')
 
     <script>
@@ -69,6 +71,28 @@
 </div>
 <!--=============SCROLL BTN==============-->
 
+
+
+<!-- Chat -->
+<div>
+<input type="checkbox" id="check">
+    <label class="chat-btn" for="check"> <i class="fa fa-commenting-o comment"></i> <i class="fa fa-close close"></i> </label>
+    <div class="wrapper">
+        <div class="header">
+            <h6>Let's Chat - Online</h6>
+        </div>
+        <div class="text-center p-2">
+            <span>Please fill out the form to start chat!</span>
+        </div>
+        <div class="chat-form">
+            <input type="text" class="form-control" placeholder="Name">
+            <input type="text" class="form-control" placeholder="Email">
+            <textarea class="form-control" placeholder="Your Text Message"></textarea>
+            <button class="btn btn-success btn-block">Submit</button>
+        </div>
+    </div>
+</div>
+<!-- End Chat -->
 
 <!--jquery library js-->
 <script src="{{asset('frontend/js/jquery-3.6.0.min.js')}}"></script>
