36682563c288f4446222a4ec6b75e4bdb0725f76
rafig256 <rafig_256@yahoo.com>
2024-05-09 11:55:56 +0330

    start create testimonial section
    create model and migration and controller

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -10,18 +10,3 @@ window.Echo.channel('message-'+cookie).listen(
             }
         }
 );
-
-window.Echo.join('online')
-    .here((users) => {
-        console.log('here');
-        console.log(users);
-    })
-    .joining((user) => {
-        console.log('joining');
-        console.log(user);
-    })
-    .leaving((user) => {
-        console.log('leaving');
-        console.log(user);
-    });
-
